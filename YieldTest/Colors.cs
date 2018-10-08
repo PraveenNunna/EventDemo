2615272d2abe97852ffeea4d048ed021bf231d2b
Nunna, Praveen <Praveen.Nunna@harman.com>
2018-10-08 18:03:36 +0200

    Add project files.

---
+++ YieldTest/Colors.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YieldTest
+{
+    public class Color
+    {
+        public string Name { get; set; }
+    }
+
+    class AllColors : IEnumerable
+    {
+        Color[] _colors =
+        {
+            new Color() { Name = "red" },
+            new Color() { Name = "blue" },
+            new Color() { Name = "green" }
+        };
+
+        public IEnumerator GetEnumerator()
+        {
+            return new ColorEnumerator(_colors);
+        }
+
+        private class ColorEnumerator : IEnumerator
+        {
+            private Color[] _colors;
+            int _position;
+
+            public ColorEnumerator(Color[] colors)
+            {
+                this._colors = colors;
+                this._position = -1;
+            }
+            public object Current
+            {
+                get
+                {
+                    return _colors[_position];
+                }
+            }
+
+            public bool MoveNext()
+            {
+                _position++;
+                if (_position >= _colors.Length)
+                    return false;
+
+                return true;
+            }
+
+            public void Reset()
+            {
+                _position = -1;
+            }
+        }
+    }
+}
---
+++ YieldTest/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace YieldTest
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var e = new Employee { Id = 1, Name = "Praveen" };
+
+            e.NameChanged += E_NameChanged1;
+            e.Name = "Praveen Nunna";
+            
+            Console.ReadKey();
+        }
+
+        private static void E_NameChanged1(object sender, EmployeeArgs e)
+        {
+            Console.WriteLine("New event args called");
+        }
+
+        private static void E_Event(object sender, EventArgs e)
+        {
+        }
+
+        private static void E_NameChanged(string newName)
+        {
+            Console.WriteLine(newName);
+        }
+
+
+        public static void PrintNewName(string newName)
+        {
+            Console.WriteLine("New name printed from method is: " + newName);
+        }
+    }
+
+
+    public class Employee
+    {
+        private string _name;
+
+        public int Id { get; set; }
+
+        public event EventHandler<EmployeeArgs> NameChanged;
+
+        public event EventHandler Event;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                OnNameChanged(value);
+                _name = value;
+            }
+        }
+
+        protected void OnNameChanged(string newName)
+        {
+            NameChanged?.Invoke(this, new EmployeeArgs());
+        }
+    }
+
+    public class EmployeeArgs : EventArgs
+    {
+        public string EmpName { get; set; }
+
+        public int Id { get; set; }
+    }
+}
